9af9d0436e8ce7db37210039d6d00540a4b91dc5
Sean <seanhc98@gmail.com>
2021-11-22 23:24:48 +0000

    Icons Updated

--- main.py
+++ main.py
@@ -33,19 +33,41 @@ editStatus = '*'
 # Window Title
 root.title('SoaPy' + ' - {}{}'.format(projectName, editStatus))
 
+# app icon
+appIcon = PhotoImage(file = 'icons/appIcon32.png') # find better method
+# appIcon = PhotoImage(r'icons/appIcon32.ico')
+
+root.tk.call('wm', 'iconphoto', root._w, appIcon)
+# root.iconbitmap(r'icons/appIcon32.ico')
+#root.iconbitmap('icons/appIcon32.ico')
+
 # Toolbar
 toolBar = Frame(root, bg='#F8F6F0')
 toolBar.pack(side=TOP, fill=X)
 # ----buttons----
 
 # Open File
-openFile = Button(toolBar, text='Open', command=openPy)
+openIcon = PhotoImage(file = r'icons/openFree.png')
+
+openFile = Button(toolBar, 
+height=20, width=20, image=openIcon,
+highlightthickness = 0, bd = 0, bg = '#F8F6F0')
+
 openFile.pack(side=LEFT, padx=2, pady= 10)
 # Save File
-saveFile = Button(toolBar, text='Save')
+saveIcon = PhotoImage(file = r'icons/saveFile20x20.png')
+
+saveFile = Button(toolBar, 
+height=20, width=20, image=saveIcon,
+highlightthickness = 0, bd = 0, bg = '#F8F6F0')
+
 saveFile.pack(side=LEFT, padx=2, pady= 10)
 # Run File
-runFile = Button(toolBar, text='Run')
+runIcon = PhotoImage(file = r'icons/playFile.png')
+runFile = Button(toolBar, 
+height=20, width=20, image=runIcon,
+highlightthickness = 0, bd = 0, bg = '#F8F6F0')
+
 runFile.pack(side=RIGHT, padx=2, pady= 10)
 
 # text field
